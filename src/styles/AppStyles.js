ecf675d78c4f44b4c1cbc687acfc34610fd0aa70
Julian-363 <juliangil333@gmail.com>
2023-08-05 17:18:03 -0500

    tarea phonebook

---
+++ src/styles/AppStyles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: block;
+  align-items: center;
+  box-sizing: content-box;
+  width: 85%;
+  border: solid darkcyan 20px;
+  border-radius: 30px ;
+  margin-top: 50px;
+  margin-left: 90px;
+  justify-content: center;
+`;
+
+export const H1 = styled.h1`
+  text-align: center;
+  margin-top: 50px;
+`;
+
+export const H2 = styled.h2`
+  text-align: center;
+  margin-top: 50px;
+`;
